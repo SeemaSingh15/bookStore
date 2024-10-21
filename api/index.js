8e07a66a0dfbdbeed73b1f6d2f5ca218ca28411e
[seema] <[seemas200415@gmail.com]>
2024-10-21 22:40:20 +0530

    connected to MongoDB successfully!

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
-import express from 'express';
-const app=express();
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
 
-app.listen(3000,() =>{
-  console.log('Server is running on port 3000');
+mongoose.connect(process.env.MONGO).then(() =>{
+  console.log('Connection to MongoDB is successful!!');
+
+}).catch((err)=>{
+console.log(err);
 });
+
+
